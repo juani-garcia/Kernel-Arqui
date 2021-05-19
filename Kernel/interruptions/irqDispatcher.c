1db9741e83b3041ce99982309a3c7bdb3ab91c27
jero_brave <jbrave271@gmail.com>
2021-05-19 14:35:48 -0300

    Add keyboard interruption to idt loader

--- Kernel/interruptions/irqDispatcher.c
+++ Kernel/interruptions/irqDispatcher.c
@@ -1,22 +1,24 @@
 #include <time.h>
-#include "keyboard.h"
+#include <keyboard.h>
 #include <stdint.h>
 
 typedef void (*PInterruption)(void);
+
 static void int_20();
-static PInterruption[255] = {&int_20, };
+static void int_21();
+
+static PInterruption interruptions[255] = {&int_20, &int_21};
 
 void int_20() {
     timer_handler();
 }
 
-
+void int_21() {
+	keyboard_handler();
+}
 
 void irqDispatcher(uint64_t irq) {
-    Pinterruption interruptions[255]={0};
-    interruptions[0] = &int_20;
-    Pinterruption interruption = interruptions[irq];
-    if (interruption != 0)
-        interruption();
+    PInterruption interruption = interruptions[irq];
+    if (interruption != 0) interruption();
     return;
 }

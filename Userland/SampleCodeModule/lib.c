1a51a9577bf0c150ea0247499a8078c72b3ff14b
jero_brave <jbrave271@gmail.com>
2021-06-02 15:30:25 -0300

    add files

---
+++ Userland/SampleCodeModule/lib.c
@@ -0,0 +1,29 @@
+#include <lib.h>
+
+int strcmp(char * str1, char * str2);
+
+int strlen(const char* str){
+    int i=0;
+    while(str[i++]);
+    return i-1;
+}
+
+int strcmp( char * str1, char * str2) {
+    while(*str1 || *str2)
+        if (*(str1++) ^ *(str2++)) return *(str2-1) - *(str1-1);
+    return 0;
+}
+
+void printf(const char * str) {
+    print(STDOUT, str, strlen(str));
+}
+
+int getchar() {
+    char c = 0;
+    read(STDIN, &c, 1);
+    return c;
+}
+
+void putchar(char c) {
+    print(STDOUT, &c, 1);
+}

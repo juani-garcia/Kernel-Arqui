a89322ce61f2fe663e9ce870a2f287d4bf7c0266
Juani Garcia <juanigarcia@itba.edu.ar>
2021-06-10 17:49:48 -0300

    My life is miserable

--- Kernel/process.c
+++ Kernel/process.c
@@ -4,31 +4,41 @@
 #include <time.h>
 #include <naiveConsole.h>
 
-static uint8_t activeProcess = 0;
+uint8_t activeProcess = 0;
+uint64_t rsps[2] = {};
 
 void load_processes(ProcessPtr p1, ProcessPtr p2) {
-    processes[0] = p1;
-    processes[1] = p2;
-    processes[0]->sp = init_process(processes[0]->stack_base, processes[0]->ip);
-    processes[1]->sp = init_process(processes[1]->stack_base, processes[1]->ip);
+    // processes[0] = p1;
+    // processes[1] = p2;
+    // processes[0]->sp = init_process(processes[0]->stack_base, processes[0]->ip);
+    // processes[1]->sp = init_process(processes[1]->stack_base, processes[1]->ip);
+    rsps[0] = init_process(200 * 1024 * 1024, 0x400000);
+    rsps[1] = init_process(300 * 1024 * 1024, 0x400000);
 }
 
 uint64_t set_up_process(uint64_t rsp) {
-    processes[activeProcess]->sp = rsp;
+    // processes[activeProcess]->sp = rsp;
+    // activeProcess = 1 - activeProcess;
+    // return processes[activeProcess]->sp;
+    rsps[activeProcess] = rsp;
     activeProcess = 1 - activeProcess;
-    return processes[activeProcess]->sp;
+    return rsps[activeProcess];
 }
 
 void begin() {
-    if(processes[0] != 0) run_process(processes[0]->sp);
+    // if(processes[0] != 0) run_process(processes[0]->sp);
+    if(rsps[0] != 0) run_process(rsps[0]);
 }
 
 void restart_process(){
-    processes[activeProcess]->sp = init_process(processes[activeProcess]->stack_base, processes[activeProcess]->ip);
+    // processes[activeProcess]->sp = init_process(processes[activeProcess]->stack_base, processes[activeProcess]->ip);
+    uint64_t sb = activeProcess == 0? 200 * 1024 * 1024 : 300 * 1024 * 1024; 
+    rsps[activeProcess] = init_process(sb, 0x400000);
 }
 
 uint64_t get_current_rsp() {
-    return processes[activeProcess]->sp;
+    // return processes[activeProcess]->sp;
+    return rsps[activeProcess];
 }
 
 uint8_t get_current_process() {
